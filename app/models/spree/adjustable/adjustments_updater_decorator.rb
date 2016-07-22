33d6ab71017f59cf72c124f95d24b4d84387a841
Filipe Moreira <code@filipemoreira.com>
2016-07-22 10:29:05 -0500

    Upgrade to Spree 3.1

--- app/models/spree/adjustable/adjustments_updater_decorator.rb
+++ app/models/spree/adjustable/adjustments_updater_decorator.rb
@@ -5,13 +5,13 @@ Spree::Adjustable::AdjustmentsUpdater.class_eval do
       super
 
       handling_total = 0
-      handling_total = adjustments.handling.reload.map(&:update!).compact.sum
+      handling_total = @adjustable.adjusters.handling.reload.map(&:update!).compact.sum
 
-      adjustable.update_columns(
+      @adjustable.update_columns(
         handling_total: handling_total,
-        adjustment_total: adjustable.adjustment_total + handling_total,
+        adjustment_total: @adjustable.adjustment_total + handling_total,
         updated_at: Time.now
-      ) unless adjustable.destroyed?
+      ) unless @adjustable.destroyed?
     end
   end
 
